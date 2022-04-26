79aeef254ecae3d486103da0f7e4a072d6089b56
Anthony Aromin <menekoiskawaii@gmail.com>
2022-04-26 23:59:47 +0800

    Change style of timer controls

---
+++ src/assets/img/icons/index.js
@@ -0,0 +1,11 @@
+import NextIcon from './NextIcon.svg';
+import PauseIcon from './PauseIcon.svg';
+import PlayIcon from './PlayIcon.svg';
+import StopIcon from './StopIcon.svg';
+
+export {
+  NextIcon,
+  PauseIcon,
+  PlayIcon,
+  StopIcon
+};
